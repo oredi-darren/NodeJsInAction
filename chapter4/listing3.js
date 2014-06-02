85e72df40f80ce8fc16aaea8a368f27b9ca9dc74
Darren Seet Serkai <darren@seet.me>
2014-06-02 17:58:50 +0900

    Completed chapter 4 listing 3

---
+++ chapter4/listing3.js
@@ -0,0 +1,25 @@
+/**
+ * Created by dseet on 6/2/2014.
+ */
+var http = require('http');
+var parse = require('url').parse;
+var join = require('path').join;
+var fs = require('fs');
+
+var root = __dirname;
+
+var server = http.createServer(function (req, res) {
+    var url = parse(req.url);
+    var path = join(root, url.pathname);
+    var stream = fs.createReadStream(path);
+    stream.on('data', function(chunk) {
+       res.write(chunk);
+    });
+    stream.on('end', function(chunk) {
+        res.end();
+    });
+});
+
+
+
+server.listen(3000);
