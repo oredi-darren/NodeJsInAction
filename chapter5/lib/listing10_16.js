bcd0f21478e1bb6c72bde0c9d5b8bb23d3b68904
Darren Seet Serkai <darren@seet.me>
2014-06-03 11:08:57 +0900

    Clean chapter 5 listing 16 to expose only CRUD actions for public API

--- chapter5/lib/listing10_16.js
+++ chapter5/lib/listing10_16.js
@@ -3,13 +3,13 @@
  */
 var qs = require('querystring');
 
-exports.sendHtml = function (res, html) {
+sendHtml = function (res, html) {
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Content-Length', Buffer.byteLength(html));
     res.end(html);
 };
 
-exports.parseReceivedData = function (req, callback) {
+parseReceivedData = function (req, callback) {
     var body = '';
     req.setEncoding('utf8');
     req.on('data', function (chunk) {
@@ -21,7 +21,7 @@ exports.parseReceivedData = function (req, callback) {
     });
 };
 
-exports.actionForm = function (id, path, label) {
+actionForm = function (id, path, label) {
     var html = '<form method = "POST" action="' + path + '">'
         + '<input type="hidden" name="id" value="' + id + '" />'
         + '<input type="submit" value="' + label + '" />'
@@ -29,41 +29,7 @@ exports.actionForm = function (id, path, label) {
     return html;
 };
 
-exports.add = function (db, req, res) {
-    exports.parseReceivedData(req, function (work) {
-        db.query("INSERT INTO work (hours, date, description) "
-                + " VALUES (?, ?, ?)"
-            , [work.hours, work.date, work.description]
-            , function (err) {
-                if (err) throw err;
-                exports.show(db, res);
-            });
-    });
-};
-
-exports.delete = function (db, req, res) {
-    exports.parseReceivedData(req, function (work) {
-        db.query("DELETE FROM work WHERE ID=?"
-            , [work.id]
-            , function (err) {
-                if (err) throw err;
-                exports.show(db, res);
-            });
-    });
-};
-
-exports.archive = function (db, req, res) {
-    exports.parseReceivedData(req, function (work) {
-        db.query("UPDATE work SET archived=1 WHERE id=?"
-            , [work.id]
-            , function (err) {
-                if (err) throw err;
-                exports.show(db, res);
-            });
-    });
-};
-
-exports.workHitlistHtml = function (rows) {
+workHitlistHtml = function (rows) {
     if(rows && rows.length > 0) {
         var html = '<table>';
         for(var i in rows) {
@@ -72,9 +38,9 @@ exports.workHitlistHtml = function (rows) {
             html += '<td>' + rows[i].hours + '</td>';
             html += '<td>' + rows[i].description + '</td>';
             if(!rows[i].archived) {
-                html += '<td>' + exports.workArchivedForm(rows[i].id) + '</td>';
+                html += '<td>' + workArchivedForm(rows[i].id) + '</td>';
             }
-            html += '<td>' + exports.workDeleteForm(rows[i].id) + '</td>';
+            html += '<td>' + workDeleteForm(rows[i].id) + '</td>';
             html += '</tr>';
             return html;
         }
@@ -82,7 +48,7 @@ exports.workHitlistHtml = function (rows) {
     }
     return '';
 };
-exports.workFormHtml = function () {
+workFormHtml = function () {
     var html ='<form method="POST" action="/">'
         + '<p>Date (YYYY-MM-DD): <br /><input type="date" name="date" /></p>'
         + '<p>Hours worked: <br /><input type="hours" name="hours" /></p>'
@@ -92,11 +58,11 @@ exports.workFormHtml = function () {
     return html;
 };
 
-exports.workArchivedForm = function (id) {
-    return exports.actionForm(id, '/archive', 'Archived');
+workArchivedForm = function (id) {
+    return actionForm(id, '/archive', 'Archived');
 };
-exports.workDeleteForm = function (id) {
-    return exports.actionForm(id, '/delete', 'Delete');
+workDeleteForm = function (id) {
+    return actionForm(id, '/delete', 'Delete');
 };
 
 exports.show = function (db, res, showArchived) {
@@ -108,12 +74,46 @@ exports.show = function (db, res, showArchived) {
             html = (showArchived)
                 ? ''
                 : '<a href="/archived">Archived Work</a><br />';
-            html += exports.workHitlistHtml(rows);
-            html += exports.workFormHtml();
-            exports.sendHtml(res, html);
+            html += workHitlistHtml(rows);
+            html += workFormHtml();
+            sendHtml(res, html);
         });
 };
 
 exports.showArchived = function (db, res) {
     exports.show(db, res, true);
+};
+
+exports.add = function (db, req, res) {
+    parseReceivedData(req, function (work) {
+        db.query("INSERT INTO work (hours, date, description) "
+                + " VALUES (?, ?, ?)"
+            , [work.hours, work.date, work.description]
+            , function (err) {
+                if (err) throw err;
+                exports.show(db, res);
+            });
+    });
+};
+
+exports.delete = function (db, req, res) {
+    parseReceivedData(req, function (work) {
+        db.query("DELETE FROM work WHERE ID=?"
+            , [work.id]
+            , function (err) {
+                if (err) throw err;
+                exports.show(db, res);
+            });
+    });
+};
+
+exports.archive = function (db, req, res) {
+    parseReceivedData(req, function (work) {
+        db.query("UPDATE work SET archived=1 WHERE id=?"
+            , [work.id]
+            , function (err) {
+                if (err) throw err;
+                exports.show(db, res);
+            });
+    });
 };
