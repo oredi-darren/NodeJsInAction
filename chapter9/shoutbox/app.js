b774a0e7ecc405fa2ad8418a6640c25b8c3e5102
Darren Seet Serkai <darren@seet.me>
2014-06-05 17:51:28 +0900

    Completed chapter 9 listing 11

--- chapter9/shoutbox/app.js
+++ chapter9/shoutbox/app.js
@@ -4,8 +4,15 @@ var path = require('path');
 
 var routes = require('./routes');
 var users = require('./routes/user');
+var register = require('./routes/register');
+
+var messages = require('./lib/messages');
 
 var app = express();
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(messages);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -14,8 +21,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
+
 app.get('/', routes.index);
 app.get('/users', users.list);
+app.get('/register', register.form);
+//app.post('/register', register.submit);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
