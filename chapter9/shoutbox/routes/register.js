7325605e967dc7bb2df89f60403de5dae2909a3e
Darren Seet Serkai <darren@seet.me>
2014-06-05 17:57:20 +0900

    Completed chapter 9 listing 11

--- chapter9/shoutbox/routes/register.js
+++ chapter9/shoutbox/routes/register.js
@@ -1,3 +1,29 @@
+var User = require('../lib/user');
+
 exports.form = function (req, res) {
     res.render('register', { title: 'Register' })
+};
+
+exports.submit = function (req, res, next) {
+    var data = req.body.user;
+    User.getByName(data.name, function (err, user) {
+        if(err) return next(err);
+
+        // redis will default it
+        if(user.id) {
+            res.error("Username already taken!");
+            res.redirect('back');
+        } else {
+            user = new User({
+                name: data.name,
+                pass: data.pass
+            });
+
+            user.save(function (err) {
+                if(err) return next(err);
+                req.session.id = user.id;
+                res.redirect('/');
+            });
+        }
+    });
 };
